b03bef9dfebb81e22fc5c28ccfa73d31a01a960b
suzeyinhappyboy <619388013@qq.com>
2016-09-14 10:53:44 +0800

    Update ScreenRecorder.java

--- ScreenRecorder/src/com/fzu/screenrecorder/ScreenRecorder.java
+++ ScreenRecorder/src/com/fzu/screenrecorder/ScreenRecorder.java
@@ -169,16 +169,30 @@ public class ScreenRecorder extends Thread {
 
     private void prepareEncoder() throws IOException {
 
+        //MediaFormat这个类是用来定义视频格式相关信息的
+        //video/avc,这里的avc是高级视频编码Advanced Video Coding
+        //mWidth和mHeight是视频的尺寸，这个尺寸不能超过视频采集时采集到的尺寸，否则会直接crash
         MediaFormat format = MediaFormat.createVideoFormat(MIME_TYPE, mWidth, mHeight);
+        //COLOR_FormatSurface这里表明数据将是一个graphicbuffer元数据
         format.setInteger(MediaFormat.KEY_COLOR_FORMAT,
                 MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+        //设置码率，通常码率越高，视频越清晰，但是对应的视频也越大，这个值我默认设置成了2000000，也就是通常所说的2M
         format.setInteger(MediaFormat.KEY_BIT_RATE, mBitRate);
+        //设置帧率，通常这个值越高，视频会显得越流畅，一般默认我设置成30，你最低可以设置成24，不要低于这个值，低于24会明显卡顿
         format.setInteger(MediaFormat.KEY_FRAME_RATE, FRAME_RATE);
+        //IFRAME_INTERVAL是指的帧间隔，这是个很有意思的值，它指的是，关键帧的间隔时间。通常情况下，你设置成多少问题都不大。
+        //比如你设置成10，那就是10秒一个关键帧。但是，如果你有需求要做视频的预览，那你最好设置成1
+        //因为如果你设置成10，那你会发现，10秒内的预览都是一个截图
         format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, IFRAME_INTERVAL);
 
         Log.d(TAG, "created video format: " + format);
+        //创建一个MediaCodec的实例
         mEncoder = MediaCodec.createEncoderByType(MIME_TYPE);
+        //定义这个实例的格式，也就是上面我们定义的format，其他参数不用过于关注
         mEncoder.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+        //这一步非常关键，它设置的，是MediaCodec的编码源，也就是说，我要告诉mEncoder，你给我解码哪些流。
+        //很出乎大家的意料，MediaCodec并没有要求我们传一个流文件进去，而是要求我们指定一个surface
+        //而这个surface，其实就是我们在上一讲MediaProjection中用来展示屏幕采集数据的surface
         mSurface = mEncoder.createInputSurface();
         Log.d(TAG, "created input surface: " + mSurface);
         mEncoder.start();
